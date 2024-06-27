27984bd205cc559a6c4e07d796c116b64068eaf6
user <user@DESKTOP-EULJV3S>
2024-06-27 12:36:31 +0530

    HIINTERVIEW

---
+++ src/inter/Pro.java
@@ -0,0 +1,23 @@
+package inter;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.TreeSet;
+
+public class Pro {
+
+	public static void main(String[] args) {
+		char[]ch= {'a','c','b','d'};
+		HashMap<Integer, Character>map=new HashMap<Integer, Character>();
+		for(int i=0;i<ch.length;i++) {
+			map.put(i, ch[i]);
+		}
+		TreeSet<Character>sort=new TreeSet<Character>();
+		
+	      for(Entry<Integer, Character> k:map.entrySet()) {
+	    	  sort.add(k.getValue());
+	      }
+	      System.out.print(sort+" ");
+	      
+	}
+}
